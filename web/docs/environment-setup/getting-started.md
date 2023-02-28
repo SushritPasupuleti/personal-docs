fd0f0052e4085d7b9e56e02c6e9adffdb70273a9
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2023-02-28 18:11:08 +0530

    feat: add environment setup instructions

---
+++ web/docs/environment-setup/getting-started.md
@@ -0,0 +1,79 @@
+---
+sidebar_position: 1
+---
+
+# Getting Started
+
+By following this guide you can setup a development environment that is similar to mine. This guide is not meant to be a one size fits all solution, but rather a starting point for you to build your own development environment.
+
+## What's included
+
+- Unix system Compatibile (WSL2, MacOS, Linux) dotfiles.
+
+- Installation scripts via Homebrew for MacOS and Linux (partially).
+
+:::info
+
+Check [Windows Setup](/docs/environment-setup/windows) for a Windows specific setup.
+
+:::
+
+### Custom Scripts
+
+Apart from the setup scripts, there are a few scripts that I created to make my life easier.
+
+- `./scripts/sp.sh` - fuzzy find and navigate to a given TMUX session.
+
+- `./scripts/sims.sh` - fuzzy find and start an iOS simulator.
+
+- `./scripts/emus.sh` - fuzzy find and start an Android simulator.
+
+## Setup
+
+Start by cloning the `dots` repository (it is not mandatory to clone this into your home directory, setup scripts will create symlinks where appropriate):
+
+```bash
+git clone https://github.com/SushritPasupuleti/dots
+```
+:::note
+
+Please do not clone this repo, it will most likely break your setup, this is customized for my usecases. Feel free to look around for inspiration though.
+
+:::
+
+follow these steps in sequence:
+
+
+### tmux
+
+Setup oh-my-tmux:
+
+```bash
+cd
+git clone https://github.com/gpakosz/.tmux.git
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+```
+
+### Run Installation Scripts
+
+Setup Applications/Utilities and setup symlinks:
+
+```bash
+chmod +x setup.sh
+chmod +x pre-setup.sh
+./pre-setup.sh
+./setup.sh
+```
+
+### Manage Exports
+
+Export Environment Variables in fish by creating a file at: `~/.config/fish/conf.d/exports.fish`
+
+```fish
+set -gx ENV_VAR "value"
+```
+
+## Wrapping Up
+
+With this, you should have a working development environment.
+
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -4,38 +4,59 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+// import Main from '@site/src/components/HomepageFeatures/main';
 
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+	const { siteConfig } = useDocusaurusContext();
+	return (
+		<header className={clsx('hero hero--primary', styles.heroBanner)}>
+			<div className="container">
+				<h1 className="hero__title">{siteConfig.title}</h1>
+				<p className="hero__subtitle">{siteConfig.tagline}</p>
+				<div className={styles.buttons}>
+					<Link
+						className="button button--secondary button--lg"
+						to="https://sushritpasupuleti.github.io/">
+						{`Check out my Portfolio/Resume 👨‍💻`}
+					</Link>
+				</div>
+			</div>
+		</header>
+	);
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+	const { siteConfig } = useDocusaurusContext();
+	return (
+		<Layout
+			title={`Hello from ${siteConfig.title}`}
+			description="Hey there, it's Sushrit, a technical and business-focused solo entrepreneur who has been building, managing, and scaling products for 7 years now. I’m quick to learn, adapt, innovate, and get scrappy when needed to ensure the product and I evolve as we should. This site will house all my documentation going forward.">
+			<HomepageHeader />
+			{/*
+			*/}
+			<main>
+				<HomepageFeatures />
+				<hr />
+				<br />
+				<section style={{
+					marginBottom: '5rem',
+				}}>
+					<div className="container">
+						<h1>{`Let's get started`}</h1>
+						<p className="hero__subtitle">{`Feel free to open a PR if you would like to correct or add to anything here :)`}</p>
+						<div className={styles.buttons}>
+							<Link
+								className="button button--secondary button--lg"
+								to="/docs/intro">
+								{`Start Browsing`}
+							</Link>
+						</div>
+					</div>
+				</section>
+				<br />
+			</main>
+		</Layout>
+	);
 }
